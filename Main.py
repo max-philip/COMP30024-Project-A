bcf2b620c7d6e99099b4027bf10bca175a399b2d
Max Philip <maxp4652@gmail.com>
2018-03-29 15:22:50 +1100

    nothing

--- Main.py
+++ Main.py
@@ -11,8 +11,7 @@ while my_board.pieces["@"]:
     my_board.make_solution_space("@")
     curr_black = my_board.pieces["@"][0]
     enemies = my_board.positions[curr_black].find_closest_enemies(my_board.pieces)
-    my_board.get_move_order(enemies, my_board.black_solution_space[curr_black][1])
-    my_board.print_board()
+    my_board.get_move_order(enemies, my_board.black_solution_space[curr_black][0])
     my_board.print_board()
 
 
