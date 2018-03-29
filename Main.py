74aa0897fa225d867b350eaf34ebf00ad7a73178
Max Philip <maxp4652@gmail.com>
2018-03-29 15:14:53 +1100

    put everything in

---
+++ Main.py
@@ -0,0 +1,37 @@
+from classes.Board import Board
+from classes.Piece import Piece
+
+game_over = False
+
+my_board = Board()
+
+my_board.print_board()
+
+while my_board.pieces["@"]:
+    my_board.make_solution_space("@")
+    curr_black = my_board.pieces["@"][0]
+    enemies = my_board.positions[curr_black].find_closest_enemies(my_board.pieces)
+    my_board.get_move_order(enemies, my_board.black_solution_space[curr_black][1])
+    my_board.print_board()
+    my_board.print_board()
+
+
+
+# my_board.make_solution_space("@")
+#
+# my_board.move_piece((6,4), (7,4))
+#
+# my_board.print_board()
+#
+# my_board.move_piece((7,4), (7,5))
+#
+# my_board.update("@")
+#
+# my_board.print_board()
+#
+# enemies = my_board.positions[(4,3)].find_closest_enemies(my_board.pieces)
+#
+#
+# my_board.get_move_order(enemies, my_board.black_solution_space[(4,3)][0])
+#
+# my_board.print_board()
