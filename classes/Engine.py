28ebc2ddc60fc8a9209262e203c157fff65792a9
Max Philip <maxp4652@gmail.com>
2018-03-30 13:47:13 +1100

    COMPLETE

--- classes/Engine.py
+++ classes/Engine.py
@@ -4,85 +4,122 @@ from classes.Piece import Piece
 
 class Engine:
 
-    CORNER = "X"
-    WHITE = "O"
-    BLACK = "@"
-    EMPTY = "-"
-
+    # Game types
     MASSACRE_TYPE = "Massacre"
     MOVES_TYPE = "Moves"
 
+    # Board object
     game_board = Board
 
+    # Engine initialiser
     def __init__(self):
         self.game_board = Board()
 
+    # Begins the game, and determines the game type
     def start(self):
-
-        self.game_board.print_board()
         if self.game_board.type == self.MASSACRE_TYPE:
-            self.massacre(self.BLACK)
+            # Run a massacre of the black pieces
+            self.massacre(Piece.BLACK)
 
         if self.game_board.type == self.MOVES_TYPE:
+            # Count and print all possible moves
             self.count_moves(self.game_board.positions)
 
+    # Counts and prints the number of possible moves that the white and black
+    # pieces can make. Whites are displayed first, then blacks are displayed
     def count_moves(self, positions):
         white_count = 0
         black_count = 0
+
+        # Iterate over all positions on the board, and counts all the possible
+        # moves that can be made
         for xy in positions.keys():
             moves = positions[xy].get_moves(xy, positions)
-            if positions[xy].type == self.WHITE:
+            if positions[xy].type == Piece.WHITE:
                 white_count += len(moves[0]) + len(moves[1])
-            if positions[xy].type == self.BLACK:
+            if positions[xy].type == Piece.BLACK:
                 black_count += len(moves[0]) + len(moves[1])
 
+        # Print the possible move counts
         print(white_count)
         print(black_count)
 
+    # Eliminates all the pieces of type "kill_type". Prints all of the moves
+    # made.
     def massacre(self, kill_type):
+
+        # Continues while there are still "kill_type" pieces on the board
         while self.game_board.pieces[kill_type]:
+
+            # Make a solution space, select the current piece, and the
+            # surrounding pieces on the x and y axes
             self.game_board.make_solution_space(kill_type)
             curr_p = self.game_board.pieces[kill_type][-1]
-            enemies = self.game_board.positions[curr_p].find_closest_enemies(self.game_board.pieces)
-            surrounding_x = self.game_board.positions[curr_p].valid_x(curr_p, self.game_board.positions)
-            surrounding_y = self.game_board.positions[curr_p].valid_y(curr_p, self.game_board.positions)
+            enemies = self.game_board.positions[curr_p].find_closest_enemies\
+                (self.game_board.pieces)
+            surround_x = self.game_board.positions[curr_p].valid_xy\
+                (curr_p, self.game_board.positions, Piece.X_AXIS)
+            surround_y = self.game_board.positions[curr_p].valid_xy\
+                (curr_p, self.game_board.positions, Piece.Y_AXIS)
+
+            # Calculates which of the possible solutions to the current
+            # "kill_type" piece are the easiest to reach, then moves the
+            # playing pieces there.
             solution_index = 0
-            if self.check_surrounding_same_type(curr_p, surrounding_x, surrounding_y)[0]:
-                solution_index = self.check_surrounding_same_type(curr_p, surrounding_x, surrounding_y)[1]
+            if self.check_surrounding_same_type(curr_p, surround_x,
+                                                surround_y)[0]:
+
+                solution_index = self.check_surrounding_same_type\
+                (curr_p, surround_x, surround_y)[1]
+
                 if solution_index == 0:
                     x_manhat = 0
                     for enemy in enemies:
-                        for sol in self.game_board.black_solution_space[curr_p][0]:
+                        for sol in self.game_board.black_sol_space[curr_p][0]:
                             x_manhat += Piece.manhattan_dist(enemy, sol)
 
                     y_manhat = 0
                     for enemy in enemies:
-                        for sol in self.game_board.black_solution_space[curr_p][-1]:
+                        for sol in self.game_board.black_sol_space[curr_p][-1]:
                             y_manhat += Piece.manhattan_dist(enemy, sol)
 
+                    # Pick based on total manhattan distance values
                     if y_manhat > x_manhat:
                         solution_index = 0
                     else:
                         solution_index = -1
+
             else:
-                self.game_board.pieces[self.BLACK].append(self.game_board.pieces[self.BLACK].pop())
+                self.game_board.pieces[Piece.BLACK].append\
+                    (self.game_board.pieces[Piece.BLACK].pop())
 
-            self.game_board.get_move_order(enemies, self.game_board.black_solution_space[curr_p][solution_index])
-            self.game_board.update(self.BLACK)
-            self.game_board.print_board()
+            self.game_board.move_in_order(enemies,
+                    self.game_board.black_sol_space[curr_p][solution_index])
 
-    def check_surrounding_same_type(self, curr_p, surrounding_x, surrounding_y):
+            self.game_board.update(Piece.BLACK)
+
+    # Checks if a piece's surrounding spaces are occupied by a piece of the
+    # same type. This allows for faster selection of which solution to go
+    # for, since, for example, two adjacent black pieces on the x axis can
+    # be immediately ruled out for a solution space on the x axis.
+    def check_surrounding_same_type(self, curr_p, surround_x, surround_y):
         has_same_neighbour = False
-        for x in surrounding_x:
-            if self.game_board.positions[x].type == self.game_board.positions[curr_p].type:
+
+        # Returns True if there is a same neighbour, and the index of the
+        # solution space to be used
+        for x in surround_x:
+            if self.game_board.positions[x].type == \
+                    self.game_board.positions[curr_p].type:
+
                 has_same_neighbour = True
 
         if not has_same_neighbour:
             return True, 0
 
         has_same_neighbour = False
-        for y in surrounding_y:
-            if self.game_board.positions[y].type == self.game_board.positions[curr_p].type:
+        for y in surround_y:
+            if self.game_board.positions[y].type == \
+                    self.game_board.positions[curr_p].type:
                 has_same_neighbour = True
 
         if not has_same_neighbour:
--- classes/Piece.py
+++ classes/Piece.py
@@ -1,34 +1,51 @@
 class Piece:
 
+    # Piece types
     CORNER = "X"
     WHITE = "O"
     BLACK = "@"
     EMPTY = "-"
 
+    X_AXIS = "x"
+    Y_AXIS = "y"
+
+    # Possible initial directions from a position
+    DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    # Object type, x and y co-ordinates
     type = str
     x = int
     y = int
 
+    # Piece initialiser
     def __init__(self, type, x, y):
         self.type = type
         self.x = x
         self.y = y
 
+    # Make a move for a piece to a new position, printing each movement made
+    # by a white piece
     def move(self, new_pos, board):
         start = (self.x, self.y)
         self.x = new_pos[0]
         self.y = new_pos[1]
+
+        # For non-empty pieces, movements are displayed
         if not self.type == Piece.EMPTY:
             print(str(start) + "->" + str(new_pos))
-        board.update(Piece.BLACK)
-        # board.print_board()
 
+        # Update black pieces after every move
+        board.update(Piece.BLACK)
+
+    # Returns True if a piece at a specified location is surrounded by a
+    # specified playing piece on either the x or y axis.
     def surrounded(self, pos, positions, kill_type):
         right = (pos[0]+1, pos[1])
         left = (pos[0]-1, pos[0])
         up = (pos[0], pos[1]-1)
         down = (pos[0], pos[1]+1)
 
+        # Checks whether an enemy is in each of the four immediate directions
         right_surr = self.check_kill_space(right, positions, kill_type)
         left_surr = self.check_kill_space(left, positions, kill_type)
         up_surr = self.check_kill_space(up, positions, kill_type)
@@ -37,42 +54,38 @@ class Piece:
         if (right_surr and left_surr) or (up_surr and down_surr):
             return True
 
+    # Returns True if a space is a "kill space" - a space that has enemies
+    # arraged such that the playing piece is eliminated if it moves into it
     def check_kill_space(self, new_pos, positions, kill_type):
         is_kill = False
 
-        if not new_pos in positions:
+        if new_pos not in positions:
             is_kill = True
-        elif (positions[new_pos].type == kill_type) or (positions[new_pos] == Piece.CORNER):
+
+        # Is a "kill space" if surrounded by enemy pieces or an enemy piece
+        # and a corner
+        elif (positions[new_pos].type == kill_type) \
+                or (positions[new_pos] == Piece.CORNER):
             is_kill = True
 
         return is_kill
 
-    def valid_x(self, pos, positions):
-        x_moves = [(-1, 0), (1, 0)]
+    # Returns a list of valid moves along either the x or y axis of a piece.
+    def valid_xy(self, pos, positions, axis):
 
-        move1 = (pos[0] + x_moves[0][0], pos[1] + x_moves[0][1])
-        move2 = (pos[0] + x_moves[1][0], pos[1] + x_moves[1][1])
+        # Set move modifiers based on axis
+        if axis == self.X_AXIS:
+            moves = [(-1, 0), (1, 0)]
 
-        if (move1 not in positions) or (move2 not in positions):
-            return []
+        if axis == self.Y_AXIS:
+            moves = [(0, -1), (0, 1)]
 
-        if positions[move1].type == Piece.CORNER:
-            if move2 in positions:
-                return [move2]
+        # Set possible moves
+        move1 = (pos[0] + moves[0][0], pos[1] + moves[0][1])
+        move2 = (pos[0] + moves[1][0], pos[1] + moves[1][1])
 
-        if positions[move2].type == Piece.CORNER:
-            if move1 in positions:
-                return [move1]
-
-        if (move1 in positions) and (move2 in positions):
-            return [move1, move2]
-
-    def valid_y(self, pos, positions):
-        y_moves = [(0, -1), (0, 1)]
-
-        move1 = (pos[0] + y_moves[0][0], pos[1] + y_moves[0][1])
-        move2 = (pos[0] + y_moves[1][0], pos[1] + y_moves[1][1])
-
+        # Check validity by whether the positions are on the board and if a
+        # corner piece is involved
         if (move1 not in positions) or (move2 not in positions):
             return []
 
@@ -87,6 +100,7 @@ class Piece:
         if move1 in positions and move2 in positions:
             return [move1, move2]
 
+    # Helper function that gets the enemy piece type for an input piece type
     def get_kill_type(self):
         if self.type == Piece.WHITE:
             return Piece.BLACK
@@ -95,25 +109,18 @@ class Piece:
         else:
             return False
 
+    # Returns True if a solution (or pair of solutions) for a location has
+    # the required pieces for deletion of a playing piece
     def check_to_delete(self, positions, sol_space, kill_type):
 
         pos = (self.x, self.y)
 
-        # print()
-        # print(pos)
-        # print(sol_space[pos])
-
+        # Must check if either axis in the solution space is entirely filled
         for axis in sol_space[pos]:
-            # print("AXIS " + str(axis))
             to_delete = True
 
-            # if axis == [] and self.next_to_corner(positions):
-            #     to_delete = False
-
             for loc in axis:
-                # print("LOC: " + str(loc))
                 if not positions[loc].type == kill_type:
-                    # print(loc)
                     to_delete = False
 
                 if not loc and self.next_to_corner(positions):
@@ -126,28 +133,32 @@ class Piece:
 
         return False
 
+    # Returns true if a position is next to a corner piece
     def next_to_corner(self, positions):
         pos = (self.x, self.y)
 
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-        for move in directions:
+        # Check if a corner piece is in any direction
+        for move in self.DIRECTIONS:
             new_pos = pos[0] + move[0], pos[1] + move[1]
             if new_pos in positions:
                 if positions[new_pos].type == Piece.CORNER:
                     return True
 
+    # Returns True if a position is currently empty (occupied by "-")
     def check_free(self, new_pos, positions):
-        return (new_pos in positions) and (positions[new_pos].type == Piece.EMPTY)
+        return (new_pos in positions) and \
+               (positions[new_pos].type == Piece.EMPTY)
 
+    # Gets a list of possible moves that a piece at a specified position can
+    # make. Returns both a valid list of moves and a list of "bad" moves
+    # that shouldn't be made (will result in the piece getting eliminated)
     def get_moves(self, pos, positions):
         valid_moves = []
 
         bad_moves = []
 
-        movements = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-        for move in movements:
+        # Checks moves that can be made in all four directions
+        for move in self.DIRECTIONS:
             a = move[0]
             b = move[1]
             new_x = pos[0] + a
@@ -155,12 +166,15 @@ class Piece:
 
             new_pos = (new_x, new_y)
 
+            # Checks if the immediate position is free
             if self.check_free(new_pos, positions):
                 if not self.surrounded(new_pos, positions, Piece.BLACK):
                     valid_moves.append(new_pos)
                 else:
                     bad_moves.append(new_pos)
 
+            # Then checks if it can make a jump move if the immediate space is
+            # currently occupied
             elif new_pos in positions:
                 new_pos = (new_pos[0] + a, new_pos[1] + b)
                 if self.check_free(new_pos, positions):
@@ -168,16 +182,21 @@ class Piece:
 
         return valid_moves, bad_moves
 
+    # Calculate the manhattan distance between two points on the board
     @staticmethod
     def manhattan_dist(start, end):
         return abs(start[0] - end[0]) + abs(start[1] - end[1])
 
+    # Finds the closest enemy pieces to a piece, returning up to two closest
+    # pieces. Returns one enemy piece if only one solution must be filled
     def find_closest_enemies(self, pieces):
         pos = (self.x, self.y)
         kill_type = self.get_kill_type()
 
         closest = []
 
+        # Goes through the current enemy pieces on the board and appends the
+        # closest pieces
         for enemy_pos in pieces[kill_type]:
             dist = self.manhattan_dist(pos, enemy_pos)
             closest.append((dist, enemy_pos))
@@ -188,6 +207,3 @@ class Piece:
             return [closest[0][1]]
         else:
             return [closest[0][1], closest[1][1]]
-
-
-
